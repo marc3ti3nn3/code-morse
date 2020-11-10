7d537e6d4f83f83ce774cb72903de500025baa6f
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-11-10 13:16:33 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.AB, function () {
+	
+})
+let liste_de_textes = []
+radio.setGroup(1)
+liste_de_textes = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
